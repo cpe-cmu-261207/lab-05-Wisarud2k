f3d118d58c89b70cfbda2167bd11388877c447e9
Wisarud2k <59276807+Wisarud2k@users.noreply.github.com>
2021-01-11 00:50:10 +0700

    course form complete allt that left is couesecard and local storage and use effect

--- src/App.js
+++ src/App.js
@@ -2,11 +2,48 @@ import { createContext, useReducer } from "react";
 import CourseCard from "./components/CourseCard";
 import CourseForm from "./components/CourseForm";
 
+export const context = createContext({})
+
+function reducer(state,action) {
+  switch(action.type){
+    case "ADD_FORM":
+      console.log("ADDform!!!")
+      return {
+        ...state,
+        mycourses:[...state.mycourses,action.payload],
+        
+      };
+    case "TEST":
+      console.log(state.mycourses);
+      return{...state
+      };
+    case "ERROR":
+      alert("Please Fill CourseId")
+      return{...state
+      };
+    }
+    
+}
+
+const initialState = {
+  mycourses:[],
+}
+
+
 function App() {
+  const [state,dispatch] = useReducer(reducer,initialState);
+  
+
   return (
     <div>
       <h1>GPA CALCULATOR</h1>
       {/* TODO ADD UI */}
+      <context.Provider value ={{state,dispatch}}>
+      <CourseForm/>
+      <button onClick={() => dispatch({type:"TEST"})} >
+        test
+      </button>
+      </context.Provider>
     </div>
   );
 }
--- src/components/CourseForm.js
+++ src/components/CourseForm.js
@@ -1,11 +1,55 @@
 import { GRADES } from "../utils/grades";
 import { CREDITS } from "../utils/credits";
 import { useContext, useState } from "react";
+import {context} from "../App";
 
 const CourseForm = () => {
+  const [input,setInput] = useState({ID:"",grade:"4",credit:"1"})
+  const {dispatch} = useContext(context);
+  
+  const addform = (e) => {
+    e.preventDefault();
+    if(input.ID !== ""){
+      dispatch({
+        type:"ADD_FORM",
+        payload: {
+          ID:input.ID,
+          grade:input.grade,
+          credit:input.grade,
+        },
+      });
+      //clear input
+      setInput({ID:"",grade:"4",credit:"1"});
+      
+    }else{
+      dispatch({
+        type:"ERROR"
+      })
+    }
+  }
+
   return (
     <>
-      <p>This is course form</p>
+      <form onSubmit = {addform}>
+        <div>
+          <input type = "text" 
+          placeholder="CourseID" 
+          onChange = {(e) => setInput({...input,ID: e.currentTarget.value})}></input>
+          <select onChange ={(e) => setInput({...input,grade: e.currentTarget.value})} >
+            {GRADES.map((item) => {
+              return <option value ={item.value} >{item.name}</option>
+            })}
+          </select>
+          <select onChange ={(e) => setInput({...input,credit: e.currentTarget.value})}>
+            {CREDITS.map((item) => {
+              return <option value = {item}> {item}</option>
+            })}
+          </select>
+          <button type="submit">
+            ADD
+          </button>
+        </div>
+      </form>
     </>
   );
 };
